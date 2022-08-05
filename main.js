466275085a401eee145328fd331d061f4f108f18
Andre Vinicius <vinicius.bastos.monteiro@gmail.com>
2022-08-05 17:34:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var navCollapse = document.querySelector('.navbar-collapse')
+var navLink = document.querySelectorAll('.nav-link')
+
+navLink.forEach((item) => {
+    item.addEventListener('click', () => {
+        navCollapse.classList.contains('.collapse')
+            ? navCollapse.classList.add('show')
+            : navCollapse.classList.remove('show')
+    })
+})
+
+
+// animando todos os items que possuirem o atributo "data-anime"
+// criando array de items
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    // identificando topo da tela e realizando calculo para renderizar a animacao
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    // varrendo array "item", verificando se o elemento esta no topo da tela
+    item.forEach((Element) => {
+        // se o elemento estiver no topo da tela adciona a class "animate" ao elemento
+        if (windowTop > Element.offsetTop) {
+            Element.classList.add("animate");
+        } else {
+            Element.classList.remove("animate");
+        }
+    });
+}
+
+animeScroll()
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+});
